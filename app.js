368fececdbf3e53521ad66548e051a6d6e052b1a
quangtienngo661 <quangtienngo661@gmail.com>
2026-05-26 08:44:00 +0700

    feat: write test and setup docker for project

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+function startServer() {
+  const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+  server.keepAliveTimeout = 120 * 1000;
+  server.headersTimeout = 120 * 1000;
+
+  return server;
+}
 
 const html = `
 <!DOCTYPE html>
@@ -59,3 +63,9 @@ const html = `
   </body>
 </html>
 `
+
+if (require.main === module) {
+  startServer();
+}
+
+module.exports = app;
